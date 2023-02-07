4d7b8bf79aec6dcc2d9d51648ee67a30a2b57f51
Andrey Makarov <makarov-andrew@inbox.ru>
2023-02-08 00:24:38 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 17_198;
+        int bonus = 20;
+
+        System.out.println(("Начислено бонусов: ") + price / bonus);
+    }
+}
