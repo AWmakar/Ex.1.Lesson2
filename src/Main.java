c59cb7778be567f8c737e0a385452e87dc239ed5
Andrey Makarov <makarov-andrew@inbox.ru>
2023-02-10 15:31:00 +0400

    ex.1.lesson3

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        int price = 17_198;
-        int bonus = 20;
-
-        System.out.println(("Начислено бонусов: ") + price / bonus);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
